cfdbbacfee331c60e9e6626357e1f73b17aece5d
hameister <joern@hameister.org>
2017-12-17 11:17:13 +0100

    Initial commit.

--- src/main/java/org/hameister/bulk/Importer.java
+++ src/main/java/org/hameister/bulk/Importer.java
@@ -1,17 +1,11 @@
 package org.hameister.bulk;
 
 import org.hameister.bulk.data.Item;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceContext;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
@@ -20,15 +14,13 @@ import java.util.UUID;
  * Created by hameister on 03.12.17.
  */
 public class Importer {
-    private EntityManagerFactory sessionFactory;
-
 
     public Importer() throws Exception {
-        setUp();
-        EntityManager entityManager = sessionFactory.createEntityManager();
+
+        EntityManager entityManager = createEntityManagerFactory().createEntityManager();
         entityManager.getTransaction().begin();
 
-            String date = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
+        String date = LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME);
 
         for (int i = 0; i < 10; i++) {
             Item item = new Item();
@@ -38,7 +30,7 @@ public class Importer {
             entityManager.persist(item);
 
             //Check when the SQL-Statements are written to the console
-            if (i==4) {
+            if (i == 4) {
                 entityManager.flush();
                 entityManager.clear();
             }
@@ -51,11 +43,8 @@ public class Importer {
     }
 
 
-    protected void setUp() throws Exception {
-        sessionFactory = Persistence.createEntityManagerFactory( "org.hibernate.tutorial.jpa" );
+    protected EntityManagerFactory createEntityManagerFactory() throws Exception {
+        return Persistence.createEntityManagerFactory("org.hibernate.tutorial.jpa");
     }
 
-    public void startImport() {
-        System.out.println("Importing....");
-    }
 }
